42bde64b01b8a338c3c657c771d9fe9d1466d70d
justinmwarner <justin@juwar.io>
2026-09-01 14:25:55 -0500

    Configure MV3 manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'Fast GitHub Review',
+    description: 'A fast review UI for GitHub pull requests.',
+    permissions: ['storage', 'tabs'],
+    host_permissions: ['https://github.com/*', 'https://api.github.com/*'],
+  },
 });
